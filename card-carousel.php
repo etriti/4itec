2502e4e531365a620471e7964cf9fbc5c789ccc0
etriti <etrithalili@gmail.com>
2017-11-08 15:11:17 +0100

    V1

---
+++ card-carousel.php
@@ -0,0 +1,96 @@
+<!-- card carousel -->
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+<div class="carousel-inner">
+<!-- first slide -->
+<div class="carousel-item active">
+<!-- row1 -->
+<div class="row">
+  <div class="col-sm-4 my-4">
+    <div class="card">
+      <img class="card-img-top" src="img/tech.jpg" alt="">
+      <div class="card-body">
+        <h4 class="card-title">Actualités</h4>
+        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus.</p>
+      </div>
+      <div class="card-footer">
+        <a href="#" class="btn btn-info">En savoir plus</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4 my-4">
+    <div class="card">
+      <img class="card-img-top" src="img/conference.jpg" alt="">
+      <div class="card-body">
+        <h4 class="card-title">Conferences</h4>
+        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus totam ut praesentium aut.</p>
+      </div>
+      <div class="card-footer">
+        <a href="#" class="btn btn-info">En savoir plus</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4 my-4">
+    <div class="card">
+      <img class="card-img-top" src="img/psa_logo.jpg" alt="">
+      <div class="card-body">
+        <h4 class="card-title">Partenaires</h4>
+        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+      </div>
+      <div class="card-footer">
+        <a href="#" class="btn btn-info">En savoir plus</a>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- /row1 -->
+</div>
+<!-- /first slide -->
+
+<!-- second slide -->
+<div class="carousel-item">
+<!-- row2 -->
+<div class="row">
+  <div class="col-sm-4 my-4">
+    <div class="card">
+      <img class="card-img-top" src="img/handshake.jpg" alt="">
+      <div class="card-body">
+        <h4 class="card-title">Actualités</h4>
+        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus.</p>
+      </div>
+      <div class="card-footer">
+        <a href="#" class="btn btn-info">En savoir plus</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4 my-4">
+    <div class="card">
+      <img class="card-img-top" src="img/calcul.jpg" alt="">
+      <div class="card-body">
+        <h4 class="card-title">Conferences</h4>
+        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus totam ut praesentium aut.</p>
+      </div>
+      <div class="card-footer">
+        <a href="#" class="btn btn-info">En savoir plus</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4 my-4">
+    <div class="card">
+      <img class="card-img-top" src="img/vw.jpg" alt="">
+      <div class="card-body">
+        <h4 class="card-title">Partenaires</h4>
+        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+      </div>
+      <div class="card-footer">
+        <a href="#" class="btn btn-info">En savoir plus</a>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- /row2 -->
+</div>
+<!-- /second slide -->
+
+</div>
+</div>
+<!-- /card carousel -->
